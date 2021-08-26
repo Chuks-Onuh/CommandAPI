ca08c8b840f26bcce95fc79f094263e01c38932c
Onuh Chukwuma. M <onuhchukwuma12@gmail.com>
2021-08-26 22:27:16 +0100

    Added model and mock repository

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CommandAPI.Data;
+using CommandAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace CommandAPI.Controllers
 {
@@ -6,6 +9,27 @@ namespace CommandAPI.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
+        private readonly ICommandAPIRepo _repository;
+        public CommandsController(ICommandAPIRepo repository)
+        {
+            _repository = repository;
+        }
 
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAllCommands();
+
+            return Ok(commandItems);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommand(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            if (commandItem == null)
+                return NotFound();
+            return Ok(commandItem);
+        }
     }
 }
