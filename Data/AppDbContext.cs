b99c951e45aecfc79a4c3fc01b1198fa6c3efab9
Onuh Chukwuma. M <onuhchukwuma12@gmail.com>
2021-08-27 01:03:01 +0100

    Added DbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CommandAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommandAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Command> CommandsItems { get; set; }
+
+    }
+}
